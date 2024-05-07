c343d409bfb535caf22a4662080545d2002ae407
Duckos-Mods <businwes@hotmail.com>
2024-05-07 10:15:58 +0100

    Pollished calculator Added structured return type handling :3

--- src/CCC.hpp
+++ src/CCC.hpp
@@ -54,6 +54,7 @@ namespace CCC
     {
         FLOAT_OR_DOUBLE     = 0, // Left most XMM0 second XMM1 third XMM2 fourth XMM3 anything after that is stack in reverse order
         INTEGER_OR_AGREGATE = 1, // Left most RCX second RDX third R8 fourth R9 anything after that is stack in reverse order
+        VOID                = 2, // Used for void types
         OTHER               = 3, 
 
     };
@@ -68,12 +69,20 @@ namespace CCC
         VariableLocation location   = VariableLocation::NONE;
         CopyType copyType           = CopyType::NONE;
         uint32_t size               = (uint32_t)-1;
-        uint32_t index              = (uint32_t)-1;
+        uint16_t index              = (uint16_t)-1;
+        bool isStructuredReturn     = false;
     };
     struct VariableProcessingData
     {
         uint32_t size   = (uint32_t)-1; // Just loops to the max
         bool isFloat    = false; 
+    };
+
+    struct FunctionData
+    {
+        std::vector<VariableData> argData   = {};
+        VariableData returnData             = {};
+
     };
 
     namespace Internal
@@ -87,6 +96,91 @@ namespace CCC
         {
             return Max - Index + Min;
         }
+
+        inline std::vector<VariableData> GetVariableDataForVariableSizes(
+            std::span<ArgumentInformation> variableSizes,
+            bool bumpRegisters = false)
+            noexcept(false)
+        {
+            std::vector<VariableData> variableData(variableSizes.size()); // Pre allocate :3
+            std::array<VariableLocation, 4> usedRegisters{ VariableLocation::NONE, VariableLocation::NONE, VariableLocation::NONE, VariableLocation::NONE };
+            uint32_t globalIndex = 0;
+            uint32_t loopCount = uint32_t((variableSizes.size() > 4) ? 4 : variableSizes.size());
+            uint32_t bumpCount = 0;
+            if (bumpRegisters)
+            {
+                if (variableData.size() == 0)
+                    variableData.resize(1); // Make sure structure return has enough space
+
+                bumpCount = 1;
+                VariableData vd{};
+                vd.size = 8;
+                vd.copyType = CopyType::ADDRESS_OF_VARIABLE;
+                vd.index = 0;
+                vd.location = VariableLocation::RCX;
+                vd.isStructuredReturn = true;
+                variableData[0] = vd;
+            }
+
+            for (uint32_t i = bumpCount; i < loopCount; i++)
+            {
+                globalIndex = i;
+                ArgumentInformation& ai = variableSizes[i];
+                VariableData vd{};
+                vd.size = (uint32_t)ai.size;
+                switch (ai.type)
+                {
+                case ArgumentType::OTHER:
+                    vd.location = Internal::IndexToRegister(false, i);
+                    vd.copyType = CopyType::ADDRESS_OF_VARIABLE;
+                    break;
+                case ArgumentType::INTEGER_OR_AGREGATE:
+                    vd.location = Internal::IndexToRegister(false, i);
+                    vd.copyType = CopyType::VALUE_OF_VARIABLE;
+                    break;
+                case ArgumentType::FLOAT_OR_DOUBLE:
+                    vd.location = Internal::IndexToRegister(true, i);
+                    vd.copyType = CopyType::VALUE_OF_VARIABLE;
+                    break;
+                default:
+                    throw std::invalid_argument("Invalid Argument Type");
+                    break;
+                }
+                vd.index = i;
+                variableData[i] = vd;
+            }
+            if (variableSizes.size() <= 4)
+                return variableData;
+
+            loopCount = uint32_t(variableSizes.size()) - 4;
+            uint32_t startIndex = loopCount + 3;
+            constexpr uint32_t lastIndex = 3;
+
+            for (uint32_t i = startIndex; i > lastIndex; i--)
+            {
+                size_t idx = Internal::RemapIndex(i, uint32_t(variableSizes.size() - 1), lastIndex + 1);
+                ArgumentInformation& ai = variableSizes[i];
+                VariableData vd{};
+                vd.size = (uint32_t)ai.size;
+                vd.location = VariableLocation::STACK;
+                vd.index = (uint32_t)i;
+                if (ai.type == ArgumentType::OTHER)
+                    vd.copyType = CopyType::ADDRESS_OF_VARIABLE; // This handles all types larger than 8 bytes
+                else
+                    vd.copyType = CopyType::VALUE_OF_VARIABLE;
+                variableData[idx] = vd;
+            }
+            return variableData;
+        }
+
+
+        inline bool GetBumpRequirementsForType(VariableProcessingData VI)
+        {
+            if (VI.size > 8)
+                return true;
+            else
+                return false;
+        }
     }
 
     template<typename... Args>
@@ -95,6 +189,11 @@ namespace CCC
     template<size_t argCount, typename... Args>
     constexpr std::array<size_t, argCount> GetVariableSizesAsArray() noexcept{return {sizeof(Args)...};}
 
+    /*
+    * @brief Gets the argument information for a types
+    * @param variableProcessingData The types to get the argument information for
+    * @return A vector of `ArgumentInformation` that contains the size and type of the variable
+    */
     inline std::vector<ArgumentInformation> GetArgumentInformationForTypes(
             std::span<VariableProcessingData> variableProcessingData
         )
@@ -115,95 +214,31 @@ namespace CCC
         }
         return VariableInformation;
     }
-
-    inline bool GetBumpRequirementsForType(VariableProcessingData VI)
+    /*
+    * @brief Gets the return info and argument info for a function
+    * @brief The first argument in the `argumentInformation` span is expected to be the structured return type if its needed
+    * @brief The return information is the first element in the argData vector. So be carefull to manage that
+    * @brief The structured return data is always a ADDRESS_OF_VARIABLE if structured return is needed
+    * @param returnInformation The return information for the function. This can be gotten from calling `GetArgumentInformationForTypes`
+    * @param argumentInformation The argument information for the function. This can be gotten from calling `GetArgumentInformationForTypes`
+    * @return A `FunctionData` struct that contains the return and argument information for the function
+    */
+    inline FunctionData GetFunctionDataForFunc(
+        ArgumentInformation returnInformation,
+        std::span<ArgumentInformation> argumentInformation
+    )
     {
-        if (VI.size > 8)
-            return true;
-        else
-            return false;
+        bool bumpRequired = (returnInformation.size > 8) ? true : false;
+        auto argInformation = Internal::GetVariableDataForVariableSizes(argumentInformation, bumpRequired);
+        FunctionData fd;
+        if (bumpRequired)
+            fd.returnData = argInformation[0];
+        fd.argData = std::move(argInformation);
+        return fd;
+
     }
 
-    inline std::vector<VariableData> GetVariableDataForVariableSizes(
-        std::span<ArgumentInformation> variableSizes,
-        bool bumpRegisters = false,
-        CallingConvention callingConvention = CallingConvention::FASTCALL) 
-        noexcept(false)
-    {
-        switch (callingConvention)
-        {
-        case CCC::CallingConvention::CDECL:
-        case CCC::CallingConvention::STDCALL:
-        case CCC::CallingConvention::FASTCALL:
-        case CCC::CallingConvention::THISCALL:
-        case CCC::CallingConvention::VECTORCALL:
-            break; // Catch the valid types
-        default:
-            throw std::invalid_argument("Invalid calling convention");
-        }
-        std::vector<VariableData> variableData(variableSizes.size()); // Pre allocate :3
-        std::array<VariableLocation, 4> usedRegisters{VariableLocation::NONE, VariableLocation::NONE, VariableLocation::NONE, VariableLocation::NONE};
-        uint32_t globalIndex = 0;
-        uint32_t loopCount = 0;
-        uint32_t bumpCount = 0;
-        if (!bumpRegisters)
-            loopCount = uint32_t((variableSizes.size() > 4) ? 4 : variableSizes.size());
-        else
-        {
-            loopCount = uint32_t((variableSizes.size() > 3) ? 3 : variableSizes.size());
-            bumpCount = 1;
-        }
-
-        for (uint32_t i = 0; i < loopCount; i++)
-        {
-            globalIndex = i;
-            ArgumentInformation& ai = variableSizes[i];
-            VariableData vd{};
-            vd.size = (uint32_t)ai.size;
-            switch (ai.type)
-            {
-            case ArgumentType::OTHER:
-                vd.location = Internal::IndexToRegister(false, i + bumpCount);
-                vd.copyType = CopyType::ADDRESS_OF_VARIABLE;
-                break;
-            case ArgumentType::INTEGER_OR_AGREGATE:
-                vd.location = Internal::IndexToRegister(false, i + bumpCount);
-                vd.copyType = CopyType::VALUE_OF_VARIABLE;
-                break;
-            case ArgumentType::FLOAT_OR_DOUBLE:
-                vd.location = Internal::IndexToRegister(true, i + bumpCount);
-                vd.copyType = CopyType::VALUE_OF_VARIABLE;
-                break;
-            default:
-                throw std::invalid_argument("Invalid Argument Type");
-                break;
-            }
-            vd.index = i;
-            variableData[i] = vd;
-        }
-        if (variableSizes.size() <= 4)
-            return variableData;
-
-        loopCount = uint32_t(variableSizes.size()) - 4;
-        uint32_t startIndex = loopCount + 3;
-        constexpr uint32_t lastIndex = 3;
-
-        for (uint32_t i = startIndex; i > lastIndex; i--)
-		{
-            size_t idx = Internal::RemapIndex(i, uint32_t(variableSizes.size() - 1), lastIndex+1);
-			ArgumentInformation& ai = variableSizes[i];
-			VariableData vd{};
-            vd.size = (uint32_t)ai.size;
-            vd.location = VariableLocation::STACK;
-            vd.index = (uint32_t)i;
-            if (ai.type == ArgumentType::OTHER)
-                vd.copyType = CopyType::ADDRESS_OF_VARIABLE; // This handles all types larger than 8 bytes
-            else
-                vd.copyType = CopyType::VALUE_OF_VARIABLE;
-            variableData[idx] = vd;
-		}
-        return variableData;
-    }
+    
 }
 
 #pragma warning(default: 4820)
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,23 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
-    std::vector<CCC::ArgumentInformation> argInfo(6);
-    for (int i = 0; i < 6; i++)
-	{
-		argInfo[i].size = 4;
-		argInfo[i].type = CCC::ArgumentType::FLOAT_OR_DOUBLE;
-	}
-    argInfo[0].type = CCC::ArgumentType::INTEGER_OR_AGREGATE;
-    argInfo[2].type = CCC::ArgumentType::INTEGER_OR_AGREGATE;
-    auto VarLocations = CCC::GetVariableDataForVariableSizes(argInfo);
+    std::vector<CCC::ArgumentInformation> argInfo(7);
+    argInfo[1].type = CCC::ArgumentType::INTEGER_OR_AGREGATE;
+    argInfo[1].size = 8;
+    argInfo[2].type = CCC::ArgumentType::FLOAT_OR_DOUBLE;
+    argInfo[2].size = 8;
+    argInfo[3].type = CCC::ArgumentType::INTEGER_OR_AGREGATE;
+    argInfo[3].size = 4;
+    argInfo[4].type = CCC::ArgumentType::FLOAT_OR_DOUBLE;
+    argInfo[4].size = 4;
+    argInfo[5].type = CCC::ArgumentType::INTEGER_OR_AGREGATE;
+    argInfo[5].size = 4;
+    argInfo[6].type = CCC::ArgumentType::FLOAT_OR_DOUBLE;
+    argInfo[6].size = 4;
+    CCC::ArgumentInformation retInfo;
+    retInfo.size = 1024;
+    retInfo.type = CCC::ArgumentType::OTHER; // Used when a void is required
+    argInfo[0] = retInfo;
+    auto FuncInfo = CCC::GetFunctionDataForFunc(retInfo, argInfo);
     return 0;
 }
